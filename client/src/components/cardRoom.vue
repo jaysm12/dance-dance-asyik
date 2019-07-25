a20b0ee72113e20ece856f179ea19fdd3146fea4
adis7102 <adis7102@gmail.com>
2019-07-25 21:22:01 +0700

    rooms

---
+++ client/src/components/cardRoom.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-card width="300" height="110" id="cardChooseRoom">
+      <v-card-title>{{ data }} Room</v-card-title>
+      <!-- <v-card-text>2 player inside</v-card-text> -->
+      <v-card-actions>
+        <v-btn text style="background-color : #FCE4EC">Dance Here</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cardRoom',
+  props: ['data']
+}
+</script>
+
+<style>
+#cardChooseRoom {
+  background-image: linear-gradient(
+    to right top,
+    #e03df0,
+    #e14cef,
+    #e258ee,
+    #e264ec,
+    #e36eeb,
+    #d977ef,
+    #d07ff3,
+    #c786f5,
+    #af8dfb,
+    #9693fe,
+    #7b98fe,
+    #5f9cfb
+  );
+}
+</style>
